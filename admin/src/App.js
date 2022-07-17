1f774861be45591aef5c874ccbff7bc4f3e62c2a
nur <medo0o6665@gmail.com>
2022-07-18 02:24:53 +0800

    set up react-router-dom from admin website

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,25 @@
+import { useContext } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { DarkModeContext } from "./context/darkModeContext";
+import "./style/dark.scss";
+
 function App() {
+  const { darkMode } = useContext(DarkModeContext);
+
   return (
-    <div>
-      <div>Working</div>
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<div>Working</div>} />
+          <Route path="login" element={<div>login</div>} />
+          <Route path="signup" element={<div>signup</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
