d274293c080e409e907b7dd7fbd795e760e28960
nur <medo0o6665@gmail.com>
2022-07-19 04:04:29 +0800

    create Widget component and modifying home page

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 import React from "react";
+import Widget from "../../components/widget/Widget";
 
 export const Home = () => {
   return (
@@ -10,10 +11,10 @@ export const Home = () => {
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          {/* <Widget type="user" /> */}
-          {/* <Widget type="order" /> */}
-          {/* <Widget type="earning" /> */}
-          {/* <Widget type="balance" /> */}
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
         </div>
         <div className="charts">
           {/* <Featured /> */}
