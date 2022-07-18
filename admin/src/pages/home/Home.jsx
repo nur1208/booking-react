8d4cb2afa182fa4140dbf2d70406118ac7842b1a
nur <medo0o6665@gmail.com>
2022-07-19 03:56:15 +0800

    create Navbar component and modifying home page

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,7 +1,32 @@
 import Sidebar from "../../components/sidebar/Sidebar";
-
+import Navbar from "../../components/navbar/Navbar";
+import "./home.scss";
 import React from "react";
 
 export const Home = () => {
-  return <Sidebar />;
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          {/* <Widget type="user" /> */}
+          {/* <Widget type="order" /> */}
+          {/* <Widget type="earning" /> */}
+          {/* <Widget type="balance" /> */}
+        </div>
+        <div className="charts">
+          {/* <Featured /> */}
+          {/* <Chart
+            title="Last 6 Months (Revenue)"
+            aspect={2 / 1}
+          /> */}
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          {/* <Table /> */}
+        </div>
+      </div>
+    </div>
+  );
 };
