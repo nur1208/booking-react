b96d3dd5bb653ed7dd43dc1cc325b37431542102
nur <medo0o6665@gmail.com>
2022-07-21 05:39:04 +0800

    update baseURL in api file

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API = (userToken) => {
   return axios.create({
-    // baseURL: `https://booking-react-backend.vercel.app/api`,
-    baseURL: `http://localhost:4050/api`,
+    baseURL: `https://booking-react-backend.vercel.app/api`,
+    // baseURL: `http://localhost:4050/api`,
     headers: { access_token: userToken },
   });
 };
