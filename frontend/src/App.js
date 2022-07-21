bd04228512c647f197755b56ac479b5c325a0dda
nur <medo0o6665@gmail.com>
2022-07-22 05:26:45 +0800

    set up react-router-dom from client website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div>working</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<div>home</div>} />
+        {/* <Route path="/hotels" element={<List />} /> */}
+        <Route path="/hotels" element={<div>list</div>} />
+        {/* <Route path="/hotels/:id" element={<Hotel />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
