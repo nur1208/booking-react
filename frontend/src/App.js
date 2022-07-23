b829d35789fb599cf7ce31ee43651e83e649ec19
nur <medo0o6665@gmail.com>
2022-07-24 00:39:15 +0800

    create HOME page client website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/" element={<div>home</div>} />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<div>home</div>} /> */}
         {/* <Route path="/hotels" element={<List />} /> */}
         <Route path="/hotels" element={<div>list</div>} />
         {/* <Route path="/hotels/:id" element={<Hotel />} /> */}
